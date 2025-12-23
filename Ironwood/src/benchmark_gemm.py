87f375c2df107cdc5dbcc1ab637327dcbedeaca0
Shuran Sun <shuran_sun@163.com>
2025-12-23 19:07:47 +0800

    fix:llo dump for simple gemm

--- Ironwood/src/benchmark_gemm.py
+++ Ironwood/src/benchmark_gemm.py
@@ -44,7 +44,7 @@ os.environ["LIBTPU_INIT_ARGS"] = (
     "--xla_tpu_accumulate_into_mrb=true "
     "--xla_tpu_scoped_vmem_limit_kib=65536 "
     "--xla_tpu_vmem_scavenging_mode=NONE "
-    "--xla_tpu_dvfs_p_state=7"
+    "--xla_tpu_dvfs_p_state=7 "
     "--xla_mosaic_dump_to=/microbenchmarks/mosaic_dumps"
 )
 
