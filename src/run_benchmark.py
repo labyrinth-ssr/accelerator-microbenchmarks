28c1f619fcdcfd8153c6f1f646fa2696a8aab941
Shuran Sun <shuran_sun@163.com>
2025-12-25 11:25:02 +0800

    add v6e matmul llo dump

--- src/run_benchmark.py
+++ src/run_benchmark.py
@@ -76,7 +76,7 @@ dtype_mapping = {
 
 # Always dump HLOs
 TMP_XLA_DUMP_DIR = "/tmp/microbenchmarks/hlo_graphs"
-os.environ["XLA_FLAGS"] = f"--xla_dump_to={TMP_XLA_DUMP_DIR} --xla_jf_dump_to=/data/microbenchmarks/v6e/llo"
+os.environ["XLA_FLAGS"] = f"--xla_dump_to={TMP_XLA_DUMP_DIR} --xla_jf_dump_to=/home/gcpuser/microbenchmarks/matmul/llo"
 
 
 def get_benchmark_config(config_path: str) -> Dict[str, Any]:
