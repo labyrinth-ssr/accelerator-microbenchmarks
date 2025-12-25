bf928f94be5bf67dd6bcb2e0245ca1f3818cce5c
Shuran Sun <shuran_sun@163.com>
2025-12-25 17:58:34 +0800

    fix mesh

--- Ironwood/src/benchmark_utils.py
+++ Ironwood/src/benchmark_utils.py
@@ -1076,7 +1076,7 @@ def create_mesh(strategy: ShardingStrategy) -> Mesh:
         mesh_axes = ("chip", "device")
         mesh = jax.sharding.Mesh(np.array(jax.devices()).reshape(mesh_shape), mesh_axes)
     elif strategy==ShardingStrategy.NO_SHARDING:
-        mesh = Mesh(np.array(jax.devices()[0]), axis_names="device")
+        mesh = Mesh(np.array([jax.devices()[0]]), axis_names="device")
     else:
         mesh = Mesh(np.array(jax.devices()), axis_names="device")
     return mesh
