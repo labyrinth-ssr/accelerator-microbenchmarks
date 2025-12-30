95e7080561be44191122d013675ea049904eacf7
Shuran Sun <shuran_sun@163.com>
2025-12-30 17:14:15 +0800

    llo dump to tmp

--- Ironwood/src/benchmark_compute.py
+++ Ironwood/src/benchmark_compute.py
@@ -59,7 +59,7 @@ os.environ["LIBTPU_INIT_ARGS"] = (
     "--xla_tpu_accumulate_into_mrb=true "
     "--xla_tpu_scoped_vmem_limit_kib=65536 "
     "--xla_tpu_dvfs_p_state=7 "
-    "--xla_jf_dump_to=/data/llo/vpu"
+    "--xla_jf_dump_to=/tmp/llo/vpu"
 )
 
 TRACE_BASE_DIR = None
