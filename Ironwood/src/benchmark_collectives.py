71ceb2884cae02accad55573caa3f7bfd8866da0
Shuran Sun <shuran_sun@163.com>
2025-12-31 17:35:47 +0800

    see llo

--- Ironwood/src/benchmark_collectives.py
+++ Ironwood/src/benchmark_collectives.py
@@ -500,22 +500,22 @@ def all_gather_benchmark(
   Returns:
     The measured time for the ICI benchmark.
   """
-  libtpu_init_args = [
-      "--xla_jf_debug_level=3",
-      "--xla_sc_disable_megacore_partitioning=true",
-      "--xla_tpu_disable_sparse_core_collective_offload_remover=true",
-      "--xla_tpu_enable_all_gather_offload_tracing=true",
-      "--xla_tpu_enable_sparse_core_collective_offload_2d_all_gather=true",
-      "--xla_tpu_enable_sparse_core_collective_offload_3d_all_gather=true",
-      "--xla_tpu_enable_sparse_core_collective_offload_all_gather=true",
-      "--xla_tpu_use_single_sparse_core_for_all_gather_offload=true",
-      "--xla_tpu_use_tc_device_shape_on_sc=true",
-      f"--xla_tpu_dvfs_p_state={GLOBAL_PSTATE}",
-      "--xla_tpu_scoped_vmem_limit_kib=65536",
-      "--xla_jf_dump_to=/tmp/llo/allgather"
-  ]
+  # libtpu_init_args = [
+  #     "--xla_jf_debug_level=3",
+  #     "--xla_sc_disable_megacore_partitioning=true",
+  #     "--xla_tpu_disable_sparse_core_collective_offload_remover=true",
+  #     "--xla_tpu_enable_all_gather_offload_tracing=true",
+  #     "--xla_tpu_enable_sparse_core_collective_offload_2d_all_gather=true",
+  #     "--xla_tpu_enable_sparse_core_collective_offload_3d_all_gather=true",
+  #     "--xla_tpu_enable_sparse_core_collective_offload_all_gather=true",
+  #     "--xla_tpu_use_single_sparse_core_for_all_gather_offload=true",
+  #     "--xla_tpu_use_tc_device_shape_on_sc=true",
+  #     f"--xla_tpu_dvfs_p_state={GLOBAL_PSTATE}",
+  #     "--xla_tpu_scoped_vmem_limit_kib=65536",
+  #     "--xla_jf_dump_to=/tmp/llo/allgather"
+  # ]
   # libtpu_init_args=[ ]
-  os.environ["LIBTPU_INIT_ARGS"] = " ".join(libtpu_init_args)
+  # os.environ["LIBTPU_INIT_ARGS"] = " ".join(libtpu_init_args)
   print("env: ", os.environ["LIBTPU_INIT_ARGS"])
   mesh = create_mesh(ici_size, mesh_shape)
 
