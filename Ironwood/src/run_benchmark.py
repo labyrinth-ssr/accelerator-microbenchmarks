850eb4fa89766e5b4753b960c850f10bf52307a6
Shuran Sun <shuran_sun@163.com>
2025-12-31 17:37:02 +0800

    see llo

--- Ironwood/src/run_benchmark.py
+++ Ironwood/src/run_benchmark.py
@@ -24,6 +24,21 @@ import ast
 import json
 import shutil
 
+os.environ["LIBTPU_INIT_ARGS"] = (
+      "--xla_jf_debug_level=3 "
+      "--xla_sc_disable_megacore_partitioning=true "
+      "--xla_tpu_disable_sparse_core_collective_offload_remover=true "
+      "--xla_tpu_enable_all_gather_offload_tracing=true "
+      "--xla_tpu_enable_sparse_core_collective_offload_2d_all_gather=true "
+      "--xla_tpu_enable_sparse_core_collective_offload_3d_all_gather=true "
+      "--xla_tpu_enable_sparse_core_collective_offload_all_gather=true "
+      "--xla_tpu_use_single_sparse_core_for_all_gather_offload=true "
+      "--xla_tpu_use_tc_device_shape_on_sc=true "
+      f"--xla_tpu_dvfs_p_state=7 "
+      "--xla_tpu_scoped_vmem_limit_kib=65536 "
+      "--xla_jf_dump_to=/tmp/llo/allgather"
+)
+
 COLLECTIVE_BENCHMARK_MAP = {
     "all_gather": "benchmark_collectives.all_gather_benchmark",
     "psum": "benchmark_collectives.psum_benchmark",
